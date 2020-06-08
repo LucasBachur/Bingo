f76546b7f22dd29326425fefb8600d703835d867
Mauro <mamo400000@gmail.com>
2020-06-08 10:34:21 -0300

    Agrego funciones tarea 1 y modifico un poco el bingo.py

--- src/bingo.py
+++ src/bingo.py
@@ -1,9 +1,18 @@
+#generar carton
+def carton():
+    carton = (
+            (1,0,0,1,1,0,1,0,1),
+            (0,1,1,1,0,1,1,1,1),
+            (0,1,0,0,1,0,0,1,0)
+    )
+    return carton
+#Funcion contar celdas
+def contar_celdas_ocupadas():
+    mi_carton= carton()
+    contador = 0
+    for fila in mi_carton:
+        for celda in fila:
+             contador = contador + celda
+    return contador
 
-def saludo():
-        return "Hola. Que tal?"
-
-def sumar(a,b):
-        return a+b
-
-def restar(a,c):
-        return a-c
+print(carton())
---
+++ test/test_celdas.py
@@ -0,0 +1,31 @@
+from src.bingo import carton
+
+#verifica que no tenga menos de 15 celdas ocupadas
+def test_menor15():
+    mi_carton= carton()
+    contador = 0
+    for fila in mi_carton:
+        for celda in fila:
+             contador = contador + celda
+
+    assert contador>=15
+
+#verifica que no tenga mas de 15 celdas ocupadas
+def test_mayor15():
+    mi_carton= carton()
+    contador = 0
+    for fila in mi_carton:
+        for celda in fila:
+             contador = contador + celda
+
+    assert contador<=15
+
+
+def test_columansocupadas():
+    mi_carton=carton()
+    revelador = True
+    for columna in range(9):
+        if(mi_carton[0][columna]== 0 and mi_carton[1][columna]== 0 and mi_carton[2][columna]==0):
+            revelador=False
+    assert revelador == True
+    
